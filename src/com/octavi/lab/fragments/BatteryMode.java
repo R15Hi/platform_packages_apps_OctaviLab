9a823dde6c0d07fe44e0be230f6f25f79191ab79
R15Hi <rk.8969009@gmail.com>
2022-04-28 11:15:53 +0530

    Battery: Added support for coloured circle icons (2/2)

--- src/com/octavi/lab/fragments/BatteryMode.java
+++ src/com/octavi/lab/fragments/BatteryMode.java
@@ -20,6 +20,9 @@ import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.R;
+import com.android.settings.Utils;
+import com.android.internal.util.octavi.OctaviUtils;
+import com.octavi.lab.preferences.SystemSettingSwitchPreference;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -30,6 +33,8 @@ public class BatteryMode extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String TAG = "XBatteryMode";
+    private static final String BATTERY_LEVEL_COLORS = "battery_level_colors";
+    private SystemSettingSwitchPreference mBatteryLevelColors;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -38,11 +43,17 @@ public class BatteryMode extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.octavi_battery_mode);
 
         ContentResolver resolver = getActivity().getContentResolver();
+        mBatteryLevelColors = (SystemSettingSwitchPreference) findPreference(BATTERY_LEVEL_COLORS);
+        mBatteryLevelColors.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         final String key = preference.getKey();
+        if (preference == mBatteryLevelColors) {
+            OctaviUtils.showSystemUiRestartDialog(getActivity());
+            return true;
+            }
         return false;
     }
 
